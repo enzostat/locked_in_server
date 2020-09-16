b2bf2f6aa0481fe6296718a72a33549c126cd7f6
Stanley Staton <stanleystaton@Stanleys-MacBook-Pro-2.local>
2020-09-15 17:06:22 -0700

    added to db model

--- locked_in_server/models.py
+++ locked_in_server/models.py
@@ -3,4 +3,7 @@ from django.db import models
 class User(models.Model):
     first_name = models.CharField(max_length=60)
     last_name = models.CharField(max_length=60)
-    
+    date_of_birth = models.DateField()
+    email = models.EmailField()
+    id_number = models.IntegerField()
+
